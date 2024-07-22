43d5bd0b92324d4d7bd19832724301a9c3a4ae8b
mohammad reza <mrghkh12@gmail.com>
2024-07-22 15:34:49 +0330

    feat(script.js): refactor all input validation

--- scipts/script.js
+++ scipts/script.js
@@ -2,74 +2,86 @@ const $ = document
 
 const formBox = $.querySelector('.formBox')
 
+const nameInput = formBox.querySelector('.fullName-input input')
+const phoneNumInput = formBox.querySelector('.phoneNum-input input')
+const emailInput = formBox.querySelector('.email-input input')
+const messagInput = formBox.querySelector('.message-input textarea')
 
-
+nameInput.addEventListener('keyup' , nameValidation)
+phoneNumInput.addEventListener('keyup' , phoneValidation)
+emailInput.addEventListener('keyup' , emailValidation)
+messagInput.addEventListener('keyup' , messageValidation)
 
 function nameValidation(){
-    if(nameInput.trim() === ''){
-        errorElem.innerHTML  = 'Name is required'
+    const nameInputError = formBox.querySelector('.fullName-input span.error')
+    
+    if(nameInput.value.trim() === ''){
+        nameInputError.innerHTML  = 'Name is required'
         return false
     } 
-    if(!nameInput.match(/^[A-Za-z]{3,}\s{1}[A-Za-z]{3,}$/)){
-        errorElem.innerHTML  = 'Write full name'
+    if(!nameInput.value.match(/^[A-Za-z]{3,}\s{1}[A-Za-z]{3,}$/)){
+        nameInputError.innerHTML  = 'Write full name'
         return false
     }
 
-    errorElem.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
+    nameInputError.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
     return true
 }
 
 
 function phoneValidation(){
+    const phoneNumInputError = formBox.querySelector('.phoneNum-input span.error')
 
-    if(phoneInput.trim() === ''){
-        errorElem.innerHTML  = 'Phone number is required'
+    if(phoneNumInput.value.trim() === ''){
+        phoneNumInputError.innerHTML  = 'Phone number is required'
         return false
     } 
-    if(phoneInput.length !== 11){
-        errorElem.innerHTML  = 'Phone number should be 11 digits'
+    if(phoneNumInput.value.length !== 11){
+        phoneNumInputError.innerHTML  = 'Phone number should be 11 digits'
         return false
     }
-    if(!phoneInput.match(/^[0-9]{11}$/)){
-        errorElem.innerHTML  = 'Only digits please'
+    if(!phoneNumInput.value.match(/^[0-9]{11}$/)){
+        phoneNumInputError.innerHTML  = 'Only digits please'
         return false
     }
 
-    errorElem.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
+    phoneNumInputError.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
     return true
 }
 
 
 function emailValidation(){
+    const emailInputError = formBox.querySelector('.email-input span.error')
 
-    if(emailInput.trim() === ''){
-        errorElem.innerHTML  = 'Email is required'
+    if(emailInput.value.trim() === ''){
+        emailInputError.innerHTML  = 'Email is required'
         return false
     } 
-    if(!emailInput.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        errorElem.innerHTML  = 'Email Invalid'
+    if(!emailInput.value.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailInputError.innerHTML  = 'Email Invalid'
         return false
     }
 
-    errorElem.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
+    emailInputError.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
     return true
 }
 
 
 function messageValidation(){
+    const messagInputError = formBox.querySelector('.message-input span.error')
 
-    let leftOver = 30 - messageInput.length
+    let leftOver = 30 - messagInput.value.length
 
-    if(messageInput.trim() === ''){
-        errorElem.innerHTML  = 'Message is required'
+    if(messagInput.value.trim() === ''){
+        messagInputError.innerHTML  = 'Message is required'
         return false
     } 
-    if(messageInput.length < 30){
-        errorElem.innerHTML  = leftOver + ' more characters required'
+    if(messagInput.value.length < 30){
+        messagInputError.innerHTML  = leftOver + ' more characters required'
         return false
     }
 
-    errorElem.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
+    messagInputError.innerHTML  = '<i class="fa-solid fa-circle-check"></i>'
     return true
 }
 
